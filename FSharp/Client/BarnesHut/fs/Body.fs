8ccdc5839c3355b930f770548538a5d2265278e0
Robert Bocchino <bocchino@me.com>
2012-12-16 19:18:05 -0500

    Revert "record type -> struct"
    RLB: I had to revert this commit, because the committed code broke BH
    (the output did not match the ref output).
    
    This reverts commit 3df61f3d58b172943c0f2dde737cf7a8a0e720a0.

--- FSharp/Client/BarnesHut/fs/Body.fs
+++ FSharp/Client/BarnesHut/fs/Body.fs
@@ -1,50 +1,51 @@
 module Body
 
-type body =
-  struct
-    val ID:int
-    val mass:double
-    val pos:Point.t
-    val vel:Point.t
-    val acc:Point.t
-    val phi:double
-    new (a : int, b :double, c : Point.t, d: Point.t, e: Point.t, f : double) =
-      {ID = a; mass = b; pos = c; vel = d; acc = e; phi = f; }
-   end
+type body = {ID:int;
+             mass:double;
+             pos:Point.t;
+             vel:Point.t;
+             acc:Point.t;
+             phi:double}
 
 
-let getID   (a : body) = a.ID;
-let getMass (a : body) = a.mass;
-let getPos  (a : body) = a.pos;
-let getVel  (a : body) = a.vel;
-let getAcc  (a : body) = a.acc;
-let getPhi  (a : body) = a.phi
+let getID   { ID=ID; mass=mass; pos=pos; vel=vel; acc=acc; phi=phi} = ID
+let getMass { ID=ID; mass=mass; pos=pos; vel=vel; acc=acc; phi=phi} = mass
+let getPos  { ID=ID; mass=mass; pos=pos; vel=vel; acc=acc; phi=phi} = pos
+let getVel  { ID=ID; mass=mass; pos=pos; vel=vel; acc=acc; phi=phi} = vel
+let getAcc  { ID=ID; mass=mass; pos=pos; vel=vel; acc=acc; phi=phi} = acc
 
 let create (ID,mass,pos,vel) =
-    new body(ID, mass, pos, vel, Point.zero, 0.0)
+    {ID=ID;
+     mass=mass;
+     pos=pos;
+     vel=vel;
+     acc=Point.zero;
+     phi=0.0}
 
 let updateMassPos (body:body) (mass',pos') =
-    create(getID body, mass', pos', getVel body)
+    { body with mass=mass';pos=pos' }
 
 let updateMassPosOpt body (mass',pos') =
     match body with
         Some body -> Some (updateMassPos body (mass',pos'))
-      | None      -> Some (create (-1, mass',pos',Point.zero))
+      | None      -> Some {ID= -1;mass=mass';pos=pos';
+                           vel=Point.zero;
+                           acc=Point.zero;phi=0.0}
 
 let updateAcc (body:body) acc' =
-    new body(getID body, getMass body, getPos body, getVel body, acc', getPhi body)
+    { body with acc=acc' }
 
 let updateVelAccPhi (body:body) (vel',acc',phi') =
-    new body(getID body, getMass body, getPos body, vel', acc', phi')
+    { body with vel=vel';acc=acc';phi=phi' }
 
 let updatePosVel (body:body) (pos',vel') =
-    new body(getID body, getMass body, pos', vel', getAcc body, getPhi body)
+    { body with pos=pos';vel=vel' }
 
 let normalize (cmr,cmv)
               body: body =
     let pos = getPos body
     let vel = getVel body
-    new body (getID body, getMass body, Point.sub(pos, !cmr), Point.sub(vel, !cmv), getAcc body, getPhi body)
+    { body with pos=Point.sub (pos,!cmr); vel=Point.sub (vel,!cmv) }
 
 let toString (body:body) =
     let ID = getID body
